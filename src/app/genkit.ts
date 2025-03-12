5c654fb00da0200f41f86bf92742a677129f9524
vipcodestudio <avipsyaifulloh.business@gmail.com>
2025-03-12 15:25:45 +0700

    feat: setup project

---
+++ src/app/genkit.ts
@@ -0,0 +1,30 @@
+'use server';
+
+import { gemini20Flash, googleAI } from '@genkit-ai/googleai';
+import { genkit, z } from 'genkit';
+
+const ai = genkit({
+  plugins: [
+    googleAI({
+      apiKey: process.env.GOOGLE_GENAI_API_KEY,
+    }),
+  ],
+  model: gemini20Flash,
+});
+
+const resultSchema = z.object({ judge: z.string(), solution: z.string() });
+
+export const generateFlow = ai.defineFlow(
+  {
+    name: 'generateFlow',
+    inputSchema: z.string(),
+  },
+  async (url) => {
+    const { output } = await ai.generate({
+      system: 'Kamu adalah kritikus yang sering memberi kritikan tajam',
+      prompt: `Buatkan saya kritik untuk akun github dengan link berikut ${url}`,
+      output: { schema: resultSchema },
+    });
+    return output;
+  },
+);
